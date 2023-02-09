bfafc4d8fde01b5f7a2a9a524a27bf5847bff806
rricol <ricol.raphael@gmail.com>
2023-02-09 22:24:45 +0100

    added webflow requirement

--- smarthome_support.js
+++ smarthome_support.js
@@ -1,25 +1,29 @@
-function openAccordion(){
-	let accordions = $('.accordion2_component');
-	for (let i=0; i<accordions.length; i++)
-	{
-		if(accordions.eq(i).find('.categorie-page_wrapper.w--current').length !== 0){
-		  accordions.eq(i).find('.accordion2_top').click();}
-	}
-  };
-  
-  function deleteMenu(){
-	  let currentProd = $('#current-product');
-	let productList = $('.product-list');
-	let idList;
-	for (let i=0; i<productList.length; i++)
-	{
-		if (currentProd[0].innerHTML == productList.eq(i).find('.navbar-dropdown1_dropdown-link')[0].innerHTML)
-	  {
-		  idList = i;
-		};
-	}
-	productList.eq(idList).remove();
-  };
+window.Webflow ||= [];
+window.Webflow.push(() => {
+	function openAccordion(){
+		let accordions = $('.accordion2_component');
+		for (let i=0; i<accordions.length; i++)
+		{
+			if(accordions.eq(i).find('.categorie-page_wrapper.w--current').length !== 0){
+			accordions.eq(i).find('.accordion2_top').click();}
+		}
+	};
+	
+	function deleteMenu(){
+		let currentProd = $('#current-product');
+		let productList = $('.product-list');
+		let idList;
+		for (let i=0; i<productList.length; i++)
+		{
+			if (currentProd[0].innerHTML == productList.eq(i).find('.navbar-dropdown1_dropdown-link')[0].innerHTML)
+		{
+			idList = i;
+			};
+		console.log(productList.eq(i).find('.navbar-dropdown1_dropdown-link').innerHTML);
+		}
+		productList.eq(idList).remove();
+	};
 
-openAccordion();
-deleteMenu();
+	openAccordion();
+	deleteMenu();
+});
